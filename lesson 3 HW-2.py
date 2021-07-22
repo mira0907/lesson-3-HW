5b603bf716e6ad3549faa9f76d1c856efb59e3e9
mira0907 <87653245+mira0907@users.noreply.github.com>
2021-07-22 08:23:00 +0800

    Add files via upload

--- lesson 3 HW-2.py
+++ lesson 3 HW-2.py
@@ -6,18 +6,23 @@ Created on Wed Jul 21 20:42:58 2021
 """
 i=0
 import random
-y=random.randint(1,10)
-print("y is "+str(y))
+y=random.randint(1,20)
 z=0
 while i<5 and z<1:
     x=input('數字(1-10)?')
     x=int(x)
     if x ==y: 
         print('you win')
+        print('你玩了'+str(i+1)+'次')
         z=1
     else:
         print('no')
         i=i+1
+        if x>y:
+            print('太大')
+        else:
+            print('太小')
 if z==0:
-    print('you lose')        
+    print('you lose')
+        
         
